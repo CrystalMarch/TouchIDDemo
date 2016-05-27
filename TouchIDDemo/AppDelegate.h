97ea9704c8f0d7f588eaa3276f62247f0cc3407c
朱慧平 <zhuhuiping@zhuhuipingdemac-mini.local>
2016-05-27 14:24:16 +0800

    Initial Commit

---
+++ TouchIDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDDemo
+//
+//  Created by 朱慧平 on 16/5/27.
+//  Copyright © 2016年 朱慧平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDDemo
+//
+//  Created by 朱慧平 on 16/5/27.
+//  Copyright © 2016年 朱慧平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
